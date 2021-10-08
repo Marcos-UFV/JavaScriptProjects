f5b9bdb17f02c8040af7a9c6be9eb490baf3ae4e
Marcos <aurelio.marcos@ufv.br>
2021-10-08 15:16:15 -0300

    budget app 1.5/2

--- budget-app/js/app.js
+++ budget-app/js/app.js
@@ -17,7 +17,7 @@ class UI {
   }
 
   //submit budget method
-  subtmitBudgetForm(){
+  submitBudgetForm(){
     const value = this.budgetInput.value;
 
     if(value === '' || value < 0){
@@ -53,13 +53,78 @@ class UI {
       this.balance.classList.add('showBlack');
     }
   }
+
+  submitExpenseForm(){
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+    if(expenseValue === '' || amountValue === '' || amountValue < 0){
+      this.expenseFeedback.classList.add('showItem');
+      this.expenseFeedback.innerHTML = `<p>values cannot be empty or negative<p>`;
+      const self = this;
+      setTimeout(function(){
+        self.expenseFeedback.classList.remove('showItem');
+      },4000);
+    }else{
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = '';
+      this.amountInput.value = '';
+
+      let expense = {
+        id:this.itemID,
+        title:expenseValue,
+        amount:amount
+      }
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      this.showBalance();
+    }
+  }
+  addExpense(expense){
+    const div = document.createElement('div');
+    div.classList.add('expense-item');
+    div.innerHTML = `
+    <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+    <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+    <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+    <div class="expense-icons list-item">
+
+     <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+      <i class="fas fa-edit"></i>
+     </a>
+     <a href="#" class="delete-icon" data-id="${expense.id}">
+      <i class="fas fa-trash"></i>
+     </a>
+    </div>
+   </div>`;
+   this.expenseList.appendChild(div);
+  }
   //total expense
   totalExpense(){
-    let total = 400;
+    let total = 0;
+    if(this.itemList.length > 0){
+      total = this.itemList.reduce(function(acc,curr){
+        acc += curr.amount;
+        return acc;
+      },0);
+    }
+    this.expenseAmount.textContent = total;
     return total;
+  }
+  //edit expense
+  editExpense(element){
+
+  }
+
+  //delete expense
+  deleteElement(element){
+
   }
 
 }
+
 
 function eventListeners(){
   const budgetForm = document.getElementById('budget-form');
@@ -71,16 +136,21 @@ function eventListeners(){
   //budget form submit
   budgetForm.addEventListener('submit',function(event){
     event.preventDefault();
-    ui.subtmitBudgetForm();
+    ui.submitBudgetForm();
   })
 
   //expense form submit
   expenseForm.addEventListener('submit',function(event){
     event.preventDefault();
+    ui.submitExpenseForm();
   })
 
-  expenseList.addEventListener('click',function(){
-
+  expenseList.addEventListener('click',function(event){
+    if(event.target.parentElement.classList.contains('edit-icon')){
+      ui.editExpense(event.target.parentElement);
+    }else if(event.target.parentElement.classList.contains('delete-icon')){
+      ui.deleteElement(even.target.parentElement);
+    }
   })
 
   
