e3b42c554a8a3eaaef4b001b8a6e8c27a19146dd
Marcos <aurelio.marcos@ufv.br>
2021-11-04 11:48:17 -0300

    End to End test example with puppeteer

--- jest_testing_basics/course2/util.test.js
+++ jest_testing_basics/course2/util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const {generateText, checkAndGenerate} = require('./util.js');
 
 test('should output be "Marcos (28 years old)"',() =>{
@@ -17,3 +18,27 @@ test('should generate a valid text output', () => {
   const text = checkAndGenerate('Marcos',28);
   expect(text).toBe('Marcos (28 years old)');
 });
+
+test('should create an element with text and correct class',async () => {
+  const browser = await puppeteer.launch({
+   
+   //tornar o primeiro atributo "true" e remover os demais, não abre o navegador
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size-1920,1080']
+  });
+
+  const page = await browser.newPage();
+  await page.goto('http://127.0.0.1:5500/index.html');
+  await page.click('input#name');
+  await page.type('input#name','Anna');
+
+  await page.click('input#age');
+  await page.type('input#age','25');
+
+  await page.click('#btnAddUser');
+
+  const finalText = await page.$eval('.user-item',el => el.textContent);
+
+  expect(finalText).toBe('Anna (25 years old)');
+},10000); //Para garantir que as chamadas assíncronas não gerem erro após 5 segundos
