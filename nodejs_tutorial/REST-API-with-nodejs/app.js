d1f48a3b3b6825685246c24dccabe32c3e4ede25
Marcos <aurelio.marcos@ufv.br>
2022-04-23 14:29:55 -0300

    Parsing the body & Handling CORS

--- nodejs_tutorial/REST-API-with-nodejs/app.js
+++ nodejs_tutorial/REST-API-with-nodejs/app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 
 const producRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Prevenindo CORS erros
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Headers','Origin,X-Requested-With, Content-Type,Accept,Authorization');
+  if(req.method == 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods','GET, PUT, POST,PATCH,DELETE')
+    return res.status(200).json({});
+  }
+  next();
+
+})
+
+
 // app.use((req,res,next)=>{
 //   res.status(200).json({
 //     message: 'It works!'
