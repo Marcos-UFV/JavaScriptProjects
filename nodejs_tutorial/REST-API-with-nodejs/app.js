673518aae23ce3cdee8aeca18cdb2825afa32021
Marcos <aurelio.marcos@ufv.br>
2022-04-23 13:29:31 -0300

    Adicionando rotas à API
    - Foram criadas as rotas para produtcs e orders
    - Ambas rotas possuem endpoints para os verbos http GET,POST e DELETE
    - A rota products ainda possui o verbo PATCH

--- nodejs_tutorial/REST-API-with-nodejs/app.js
+++ nodejs_tutorial/REST-API-with-nodejs/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 
+const producRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
 
-app.use((req,res,next)=>{
-  res.status(200).json({
-    message: 'It works!'
-  });
-})
+
+
+// app.use((req,res,next)=>{
+//   res.status(200).json({
+//     message: 'It works!'
+//   });
+// })
+
+app.use('/products',producRoutes);
+app.use('/orders',ordersRoutes);
 
 module.exports = app;
