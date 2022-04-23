012412218988adaf592e929876048b8cc9bca28e
Marcos <aurelio.marcos@ufv.br>
2022-04-23 13:50:36 -0300

    Lidando com erros e melhorando a configuração do projeto
    - Utilizando nodemon para reiniciar o servidor automaticamente no ambiente de desenvolvimento (--save-dev nodemon)
    - Inserido um log padrão para todas requisições que chegam por meio da lib morgan
    - Lindando com error de rotas que não são encontradas, provendo a mensagem de erro adequada.

--- nodejs_tutorial/REST-API-with-nodejs/app.js
+++ nodejs_tutorial/REST-API-with-nodejs/app.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const producRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
 
-
+app.use(morgan('dev'));
 // app.use((req,res,next)=>{
 //   res.status(200).json({
 //     message: 'It works!'
 //   });
 // })
 
+
+// Routes which should handle requests
 app.use('/products',producRoutes);
 app.use('/orders',ordersRoutes);
+
+app.use((req,res,next)=>{
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+// Esta parte é responsável por pegar tanto os erros de rotas não encontradasa quanto erros que podem
+//ocorrer dentro das rotas, por exemplo um erro de banco de dados
+app.use((error,req,res,next)=>{
+  res.status(error.status || 500);
+  res.json({
+    error:{
+      message: error.message
+    }
+  })
+});
 
 module.exports = app;
