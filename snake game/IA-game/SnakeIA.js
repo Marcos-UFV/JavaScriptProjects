765eb07c2e94063fe9148c4b1b59d43978a57d0d
Marcos <aurelio.marcos@ufv.br>
2023-04-06 10:31:34 -0300

    Adaptando o jogo
    - A partir de agora é possível que um Agente jogue
    - Foram removidas as interações com o mouse
    - O Agente externo informa os mivimentos para o personagem (cobra)

---
+++ snake game/IA-game/SnakeIA.js
@@ -0,0 +1,194 @@
+window.onload = ()=>{
+
+  const game = new SnakeGame(640,480);
+  let count = 0;
+  setInterval(()=>{
+    
+    let action = Action[count % 10];
+    count++;
+    let [gameOver,score,reward] = game.playStep(action);
+    console.log(gameOver,score,reward);
+    if(gameOver) game.reset();
+  },120);
+
+}
+const Action = [
+ [1,0,0],
+ [1,0,0],
+ [1,0,0],
+ [1,0,0],
+ [0,0,1],
+ [1,0,0],
+ [1,0,0],
+ [1,0,0],
+ [1,0,0],
+ [1,0,0]
+]
+const Direction = {
+  RIGHT:1,
+  LEFT:2,
+  UP:3,
+  DOWN:4
+}
+const Colors ={
+  WHITE:"'#000000'",
+  RED:"'#FF0000'",
+  BLUE1:"'#0000FF'",
+  BLUE2:"'#0064FF'",
+  BLACK:"'#000000'"
+}
+const BLOCK_SIZE = 20;
+class SnakeGame{
+  
+  constructor(width,height){
+    this.stage = document.querySelector("#stage");
+    this.ctx = this.stage.getContext("2d");  
+    this.w = width;
+    this.h = height;
+
+    // Init display
+    // document.onkeydown = this.keyPush.bind(this);
+    
+
+    this.reset();
+
+   
+  }
+
+  reset(){
+    // Init game state
+    this.direction = Direction.RIGHT;
+    
+    this.head = {x:this.w/2,y:this.h/2}
+    this.snake = [this.head,{x:this.head.x-BLOCK_SIZE,y:this.head.y},{x:this.head.x-(2*BLOCK_SIZE),y:this.head.y}];
+    this.score = 0;
+    this.gameOver = false;
+    this.food = null;
+    this._placeFood();
+    this.frameIteration = 0;
+  }
+  _updateUI(){
+    this.ctx.fillStyle = "#000";
+    this.ctx.fillRect(0,0,this.w,this.h);
+    
+    for(const pt of this.snake){
+      this.ctx.fillStyle = "#0000FF";
+      this.ctx.fillRect(pt.x,pt.y,BLOCK_SIZE,BLOCK_SIZE);
+      this.ctx.fillStyle = "#0064FF";
+      this.ctx.fillRect(pt.x+4,pt.y+4,12,12);
+    }
+    this.ctx.fillStyle = "#FF0000";
+    this.ctx.fillRect(this.food.x,this.food.y,BLOCK_SIZE,BLOCK_SIZE);
+  }
+  _placeFood(){
+    let x = Math.floor((Math.random()*((this.w - BLOCK_SIZE)/BLOCK_SIZE)))*BLOCK_SIZE;
+    let y = Math.floor((Math.random()*((this.h - BLOCK_SIZE)/BLOCK_SIZE)))*BLOCK_SIZE;
+    this.food = {x:x,y:y};
+    for(let i= 0; i < this.snake.length;i++){
+      if(this.food.x == this.snake[i].x && this.food.y == this.snake[i].y) this._placeFood();
+    }
+  }
+  playStep(action) {
+    this.frameIteration +=1;
+    // console.log(this.snake.splice(1));
+    // 1.collect user input
+    
+    // 2. move
+    this._move(action); // update the head
+    this.snake.unshift(this.head);
+   
+    // 3. check if game over
+    let reward = 0;
+    if(this._isColision() || this.frameIteration > 100*this.snake.length){
+      this.gameOver = true;
+      reward = -10;
+      return [this.gameOver,this.score,reward];
+    }
+    // 4. place new food or just move
+    if(this.head.x == this.food.x && this.head.y == this.food.y){
+      this.score += 1;
+      reward = 10;
+      this._placeFood();
+    }else{
+      this.snake.pop();
+    }
+    // 5. update ui
+    this._updateUI();
+    // 6. return game over and score
+    
+    return [this.gameOver,this.score,reward];
+  }
+  _isColision(pt=null){
+    if(pt == null) pt =this.head;
+    // hits boundary
+    if(pt.x > this.w - BLOCK_SIZE || pt.x < 0 || pt.y > this.h - BLOCK_SIZE || pt.y < 0) 
+      return true;
+    // hits itself
+    // if(this.head in this.snake.splice(1)){
+    //   return true;
+    // }
+    let colision = false;
+    for(let i=1; i < this.snake.length; i++){
+      colision = (pt.x == this.snake[i].x && pt.y == this.snake[i].y);
+      if(colision) break; 
+    }
+
+    return false || colision;
+  }
+  _move(action){
+    // [straight,right,left]
+
+    let clockWise =[Direction.RIGHT, Direction.DOWN,Direction.LEFT,Direction.UP];
+    let idx = clockWise.indexOf(this.direction);
+    let newDir = null;
+    if([1,0,0].every((e,i)=> e == action[i])){
+      newDir = clockWise[idx]; // No change
+    }else if([0,1,0].every((e,i)=> e == action[i])){      
+      let nextIndex = this._remainder(idx + 1,4);
+      newDir = clockWise[nextIndex]; // right turn r -> d -> l -> u
+    }else{ // [0,0,1]
+      let nextIndex = this._remainder(idx - 1,4);
+      newDir = clockWise[nextIndex]; // left turn r -> u -> l -> d
+    }
+    this.direction = newDir;
+  
+    let x = this.head.x;
+    let y = this.head.y;
+
+    switch (this.direction) {
+      case Direction.LEFT:
+        x -= BLOCK_SIZE;
+        break;
+      case Direction.UP:
+        y -= BLOCK_SIZE;
+        break;
+      case Direction.RIGHT:
+        x += BLOCK_SIZE;
+        break;
+      case Direction.DOWN:
+        y += BLOCK_SIZE;
+        break;
+    }
+    this.head = {x:x,y:y};
+  }
+  _remainder(x,r){
+    return (x - Math.floor((x)/r)*r);
+  }
+  keyPush(e){
+    switch (e.keyCode) {
+      case 37:
+        this.direction = Direction.LEFT;
+        break;
+      case 38:
+        this.direction = Direction.UP;
+        break;
+      case 39:
+        this.direction = Direction.RIGHT;
+        break;
+      case 40:
+        this.direction = Direction.DOWN;
+        break;
+    }
+  }
+  
+}
